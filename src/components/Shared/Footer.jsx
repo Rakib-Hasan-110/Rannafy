8ba9193a89fc086a13b1472cd3fc64ba158d610a
Rakib Hasan 110 <rakibhasanofficials110@gmail.com>
2026-05-31 20:58:55 +0600

    Update components

---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,120 @@
+import { Mail, Facebook, Instagram, Twitter } from "lucide-react";
+import { Link } from "react-router";
+import Logo from "../Shared/Logo";
+
+export default function Footer() {
+  return (
+    <div className="bg-gray-900 text-gray-300 md:w-11/12 mx-auto md:rounded-t-4xl">
+      <div className="max-w-7xl mx-auto px-6 py-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
+          {/* Brand and Tagline */}
+          <div className="md:col-span-1">
+            <div className="flex items-center gap-3 mb-4">
+              <Logo />
+            </div>
+            <p className="text-gray-400 leading-relaxed">
+              Your daily dose of delicious recipes made by real home cooks —
+              just like you.
+            </p>
+            <p className="text-primary font-semibold mt-4">
+              Cook. Share. Enjoy.
+            </p>
+          </div>
+
+          {/* Quick Links */}
+          <div>
+            <h3 className="text-xl font-bold text-white mb-6">Quick Links</h3>
+            <ul className="space-y-3">
+              <li>
+                <Link to="/about" className="hover:text-primary transition">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="hover:text-primary transition">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="hover:text-primary transition">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="hover:text-primary transition">
+                  Terms of Service
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Working Hours */}
+          <div>
+            <h3 className="text-xl font-bold text-white mb-6">Working Hours</h3>
+            <ul className="space-y-3">
+              <li className="hover:text-primary transition">
+                <span className="text-primary font-bold">Open: </span> Friday -
+                Saturday
+              </li>
+              <li className="hover:text-primary transition">
+                <span className="text-primary font-bold">Close: </span> No Close
+              </li>
+              <li className="text-primary font-bold">Service 24/7</li>
+            </ul>
+          </div>
+
+          {/* Newsletter and social icons */}
+          <div>
+            <h3 className="text-xl font-bold text-white mb-6">
+              Stay Connected
+            </h3>
+            <p className="text-gray-400 mb-4">
+              Get weekly recipe inspiration in your inbox
+            </p>
+
+            <div className="flex gap-3 mb-8">
+              <input
+                type="email"
+                placeholder="Your email"
+                className="flex-1 px-5 py-3 rounded-full bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary w-2/4 md:w-full"
+              />
+              <button className="bg-primary hover:bg-primary text-white p-3 rounded-full transition cursor-pointer">
+                <Mail size={20} />
+              </button>
+            </div>
+
+            {/* Social Icons */}
+            <div className="flex gap-5">
+              <a
+                href="https://secure.facebook.com/login/"
+                className="w-12 h-12 bg-gray-800 rounded-full flex items-center justify-center hover:bg-orange-600 transition"
+              >
+                <Facebook size={20} />
+              </a>
+              <a
+                href="https://www.instagram.com/"
+                className="w-12 h-12 bg-gray-800 rounded-full flex items-center justify-center hover:bg-orange-600 transition"
+              >
+                <Instagram size={20} />
+              </a>
+              <a
+                href="https://twitter.com/"
+                className="w-12 h-12 bg-gray-800 rounded-full flex items-center justify-center hover:bg-orange-600 transition"
+              >
+                <Twitter size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Bar */}
+        <div className="border-t border-gray-800 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500">
+          <p>
+            &copy; 2026 <span className="text-primary"> RannaFy.</span> All
+            rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Shared/Header.jsx
@@ -0,0 +1,229 @@
+import { useState, useRef, useEffect, useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
+import { Menu, X, LogOut, LayoutDashboard, UserCircle } from "lucide-react";
+import Logo from "./Logo";
+import { AuthContext } from "../../Context/AuthContext";
+import Swal from "sweetalert2";
+const Header = () => {
+  const { user, logOut } = useContext(AuthContext);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const userDropdownRef = useRef(null);
+
+  const handleSingOut = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `${user.displayName}. If you're logging out, you can't access!`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#f54a00",
+      cancelButtonColor: "#ff0000",
+      confirmButtonText: "Logout",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        logOut()
+          .then(() => {
+            navigate("/login");
+          })
+          .catch((error) => console.log(error));
+        Swal.fire({
+          title: "Logout successful",
+          text: "Thanks for staying with RannaFy!",
+          icon: "success",
+        });
+      }
+    });
+  };
+
+  // Public menu
+  const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Meals", path: "/meals" },
+    { name: "Contact", path: "/contact" },
+    ...(user ? [{ name: "Dashboard", path: "/dashboard" }] : []),
+  ];
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        userDropdownRef.current &&
+        !userDropdownRef.current.contains(event.target)
+      ) {
+        setUserDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const isActive = (path) => {
+    if (path === "/") return location.pathname === "/";
+    return location.pathname.startsWith(path);
+  };
+
+  return (
+    <div className="w-11/12 mx-auto">
+      <div className="flex items-center justify-between h-16 md:h-20">
+        <Link to="/" className="flex items-center gap-2">
+          <Logo />
+        </Link>
+
+        {/* Desktop Navigation */}
+        <nav className="hidden md:flex space-x-5">
+          {menuItems.map((item) => {
+            return (
+              <Link
+                key={item.name}
+                to={item.path}
+                className={`flex items-center transition-colors font-medium ${
+                  isActive(item.path)
+                    ? "text-orange-500"
+                    : "text-gray-700 hover:text-orange-500"
+                }`}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </nav>
+
+        {/* Right  */}
+        <div className="flex items-center space-x-4">
+          <div className="hidden sm:block relative" ref={userDropdownRef}>
+            {user ? (
+              <>
+                <button
+                  onClick={() => setUserDropdownOpen(!userDropdownOpen)}
+                  className="p-2 text-gray-700 hover:text-orange-500 transition-colors cursor-pointer"
+                >
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    alt="User Avatar"
+                    src={
+                      user?.photoURL || "https://i.ibb.co/8xM1d0B/avatar.png"
+                    }
+                  />
+                </button>
+
+                {userDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg py-2 border border-gray-200">
+                    <Link
+                      to="/dashboard"
+                      className="flex items-center w-full cursor-pointer px-4 py-2 text-gray-700 hover:bg-orange-50 hover:text-orange-500 rounded-lg transition-colors"
+                    >
+                      <UserCircle className="w-5 h-5 mr-3" />
+                      Profile
+                    </Link>
+                    <button
+                      onClick={handleSingOut}
+                      className="w-full flex items-center px-4 py-2 hover:bg-orange-50 text-orange-500 transition-colors"
+                    >
+                      <LogOut className="w-5 h-5 mr-3" />
+                      Logout
+                    </button>
+                  </div>
+                )}
+              </>
+            ) : (
+              <div className="space-x-2">
+                <Link
+                  to="/login"
+                  className="bg-orange-500 text-white px-4 py-2 rounded font-semibold"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/registration"
+                  className="border border-orange-500 text-orange-500 px-4 py-2 rounded font-semibold"
+                >
+                  Signup
+                </Link>
+              </div>
+            )}
+          </div>
+
+          {/* Mobile Button */}
+          <button
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            className="md:hidden p-2 text-gray-700 hover:text-orange-500 transition-colors"
+          >
+            {mobileMenuOpen ? (
+              <X className="w-6 h-6" />
+            ) : (
+              <Menu className="w-6 h-6" />
+            )}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {mobileMenuOpen && (
+        <div className="md:hidden py-4 border-t border-gray-200">
+          <nav className="flex flex-col space-y-3">
+            {menuItems.map((item) => {
+              return (
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={`flex items-center px-4 py-2 rounded-lg transition-colors font-medium ${
+                    isActive(item.path)
+                      ? "bg-orange-50 text-orange-500"
+                      : "text-gray-700 hover:bg-orange-50 hover:text-orange-500"
+                  }`}
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+
+            {/* User items in mobile */}
+            {user ? (
+              <div className="border-t border-gray-200 pt-3 mt-3">
+                <Link
+                  to="/dashboard"
+                  className="flex items-center w-full cursor-pointer px-4 py-2 text-gray-700 hover:bg-orange-50 hover:text-orange-500 rounded-lg transition-colors"
+                >
+                  <UserCircle className="w-5 h-5 mr-3" />
+                  Profile
+                </Link>
+                <button
+                  onClick={handleSingOut}
+                  className="flex items-center w-full cursor-pointer px-4 py-2 text-gray-700 hover:bg-orange-50 hover:text-orange-500 rounded-lg transition-colors"
+                >
+                  <LogOut className="w-5 h-5 mr-3" />
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div className="mt-6 flex flex-col gap-3 text-center">
+                <Link
+                  to="/login"
+                  onClick={() => setMobileMenuOpen(false)}
+                  className="bg-orange-500 text-white px-4 py-2 rounded font-semibold w-full"
+                >
+                  {" "}
+                  Login{" "}
+                </Link>
+                <Link
+                  to="/registration"
+                  onClick={() => setMobileMenuOpen(false)}
+                  className="border border-orange-500 text-orange-500 px-4 py-2 rounded font-semibold"
+                >
+                  Signup
+                </Link>
+              </div>
+            )}
+          </nav>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
